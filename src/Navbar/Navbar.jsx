27490adad34efbcf7efa94e8bd68fabdef5f0ec6
fedderica <fedderica.bartoli@gmail.com>
2023-03-27 14:21:21 -0300

    INICIO-project

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Navbar.css";
+
+
+const Navbar = () => {
+    return(
+    <div className="navbar">
+<div className="nav_logo"></div>
+<div className="nav_items">
+    <href> INICIO</href>
+    <href> BIO </href>
+    <href> PORTAFOLIO </href>
+    <href> CONTACTO </href>
+</div>
+<div className="nav_toggle">
+    <span><span>
+    <span></span>
+    </span></span>
+</div>
+    </div>
+    )
+}
+
+export default Navbar
